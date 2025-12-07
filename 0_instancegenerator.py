df74667e9fad449b948e1d2dc310d1f674046137
tobiasderoos <tobiasderoos@gmail.com>
2025-12-07 20:33:47 +0100

    changes

---
+++ 0_instancegenerator.py
@@ -0,0 +1,40 @@
+import numpy as np
+import random
+import os
+
+
+def generate_instance(n):
+    weight = [random.randint(1, 100) for i in range(n)]
+    profit = [[random.randint(1, 100) for i in range(n)] for j in range(n)]
+    for i in range(n):
+        for j in range(i, n):
+            profit[j][i] = profit[i][j]
+    cap = random.randint(round(0.4 * sum(weight)), round(0.6 * sum(weight)))
+
+    return cap, weight, profit
+
+
+if __name__ == "__main__":
+    # Parameters
+    n = 50
+    seed = 400
+    np.random.seed(seed)
+    dir = "InstancesEx1_50_test500"
+    os.makedirs(dir, exist_ok=True)
+
+    for it in range(500):
+        # Generate instance
+        c, w, p = generate_instance(n)
+
+        # Print instance to file
+        filename = f"{dir}/instance{it}.txt"
+        with open(filename, "w") as f:
+            f.write(f"{n}\n")
+            f.write(f"{c}\n")
+            for i in w:
+                f.write(f"{i}\t")
+            f.write("\n")
+            for i in range(n):
+                for j in range(n):
+                    f.write(f"{p[i][j]}\t")
+                f.write("\n")
